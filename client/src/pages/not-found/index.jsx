d7414449a8ed06f500121135fb742c42b43ec098
shreyash Jadhav <connectshreyashj96@gmail.com>
2026-02-06 17:25:24 +0530

    feat: setup shadcn/ui with Tailwind configuration

--- client/src/pages/not-found/index.jsx
+++ client/src/pages/not-found/index.jsx
@@ -1,7 +1,14 @@
+import { Button } from "@/components/ui/button"
+
 function NotFound() {
     return (
-        <div>
+        <div >
             page doesn't exists
+            <div className="flex min-h-svh flex-col items-center justify-center">
+
+                 {/* Adding this button for testing purpose shadcn  */}
+                <Button>Click me</Button>
+            </div>
         </div>
     );
 }
