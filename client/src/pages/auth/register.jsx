fb0748900272eee561a21fe92eebc376c50e65b3
shreyash Jadhav <connectshreyashj96@gmail.com>
2026-02-10 17:50:23 +0530

    feat: collect form values and create object

--- client/src/pages/auth/register.jsx
+++ client/src/pages/auth/register.jsx
@@ -13,26 +13,28 @@ const AuthRegister = () => {
 
     const [formData, setFormData] = useState(initialState)
 
-    function onSubmit() {
-
+    function onSubmit(e) {
+        e.preventDefault()
+        console.log(formData)
     }
 
-    return <div className='mx-auto w-full max-w-md space-y-6'>
-        <div className='text-center'>
-            <h1 className='text-3xl font-bold tracking-tight text-foreground'>Create new account</h1>
-            <p className='mt-2'>Already have an account
-                <Link to="/auth/login" className='font-medium ml-2 text-primary hover:underline'>Login</Link>
-            </p>
+    return (
+        <div className='mx-auto w-full max-w-md space-y-6'>
+            <div className='text-center'>
+                <h1 className='text-3xl font-bold tracking-tight text-foreground'>Create new account</h1>
+                <p className='mt-2'>Already have an account
+                    <Link to="/auth/login" className='font-medium ml-2 text-primary hover:underline'>Login</Link>
+                </p>
+            </div>
+            <CommonForm
+                formControls={registerFormControls}
+                buttonText={'Sign Up'}
+                formData={formData}
+                setFormData={setFormData}
+                onSubmit={onSubmit}
+            />
         </div>
-        <CommonForm
-            formControls={registerFormControls}
-            buttonText={'Sign Up'}
-            formData={formData}
-            setFormData={setFormData}
-            onSubmit={onSubmit}
-        />
-    </div>
-
+    )
 }
 
 export default AuthRegister
