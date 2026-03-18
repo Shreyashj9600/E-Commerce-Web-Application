5b71302ab1eca6c5f95985b15a76cc4b9f7d536b
shreyash Jadhav <connectshreyashj96@gmail.com>
2026-03-18 17:00:06 +0530

    Add search products UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,6 +22,7 @@ import { checkAuth } from "./store/auth-slice";
 import { Skeleton } from "@/components/ui/skeleton"
 import PaypalReturnPage from "./pages/shopping-view/paypal-return";
 import PaymentSuccessPage from "./pages/shopping-view/payment-success";
+import SearchProducts from "./pages/shopping-view/search";
 
 function App() {
   // testing check auth 
@@ -69,6 +70,7 @@ function App() {
           <Route path="account" element={<ShoppingAccount />} />
           <Route path="paypal-return" element={<PaypalReturnPage />} />
           <Route path="payment-success" element={<PaymentSuccessPage />} />
+          <Route path="search" element={<SearchProducts />} />
         </Route>
         <Route path="*" element={<NotFound />}></Route>
         <Route path="/unauth-page" element={<UnauthPage />}></Route>
