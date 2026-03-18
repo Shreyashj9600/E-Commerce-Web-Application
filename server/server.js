6ba09d38ecc80a438777ea89cefbbcf05ba8185a
shreyash Jadhav <connectshreyashj96@gmail.com>
2026-03-18 11:03:23 +0530

    Add admin order management APIs

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const authRouter = require('./routes/auth/auth-routes')
 const adminProductsRouter = require("./routes/admin/products-routes");
+const adminOrderRouter = require("./routes/admin/order-routes");
+
 const shopProductsRouter = require('./routes/shop/products-routes')
 const shopCartRouter = require('./routes/shop/cart-routes')
 const shopAddressRouter = require("./routes/shop/address-routes");
@@ -42,13 +44,15 @@ app.get('/', (req, res) => {
     res.json({
         success: true,
         message: 'api is running'
-    })                                 
+    })
 })
 
 app.use(cookieParser())
 app.use(express.json())
 app.use('/api/auth', authRouter)
 app.use("/api/admin/products", adminProductsRouter);
+app.use("/api/admin/orders", adminOrderRouter);
+
 app.use('/api/shop/products', shopProductsRouter)
 app.use('/api/shop/cart', shopCartRouter)
 app.use("/api/shop/address", shopAddressRouter);
